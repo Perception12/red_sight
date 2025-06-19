4ee348ed13d9d53584da5a87312a9bb60e8580e1
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2025-06-19 17:29:10 +0100

    update metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover and Share AI Prompts",
+  title: "RedSight",
+  description: "Monitor and manage traffic violations in real-time",
 };
 
 const RootLayout = ({ children }) => {
